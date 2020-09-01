b818a94f997508a8eb6df48f8a1173e072ed748a
igorpereirabr1 <igor.pereira.br@gmail.com>
2020-09-01 10:45:29 -0300

    Adjust version

--- hermione_databricks/__init__.py
+++ hermione_databricks/__init__.py
@@ -1,4 +1,3 @@
 from .cli import *
 from .project import *
 from .version import __version__
-#__author__ = 'A3Data'
--- hermione_databricks/version.py
+++ hermione_databricks/version.py
@@ -1,4 +1,4 @@
-__version__ = '0.0.7'
+__version__ = '0.0.8'
 #Version :0
 #Release :0
-#Revision:7
+#Revision:8
