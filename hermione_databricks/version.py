f2faf224e31f7259b496486bdab151ce9c04b0ae
igorpereirabr1 <igor.pereira.br@gmail.com>
2020-09-10 10:37:49 -0300

    Update project version

--- hermione_databricks/version.py
+++ hermione_databricks/version.py
@@ -1,4 +1,12 @@
-__version__ = '0.0.13'
+__version__ = '1.0.0'
 #Version :0
 #Release :0
 #Revision:13
+
+"""
+VERSÃO: Acrescenta-se um numero a VERSÃO se o projeto for uma evolução.
+
+RELEASE: Acrescenta-se um numero a RELEASE se o projeto for uma melhoria (ex: exclusão ou inclusão de novas funcionalidades).
+
+REVISÃO: Acrescenta-se um numero a REVISÃO se o projeto for uma correção de funcionalidades já existentes.
+"""
