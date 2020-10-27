565f20795e827cbe756f8acb6ae8c96ee81c9210
igorpereirabr1 <igor.pereira.br@gmail.com>
2020-10-27 17:43:06 -0300

    BugFix 273523763273

--- hermione_databricks/api/test.py
+++ hermione_databricks/api/test.py
@@ -1,1 +1,3 @@
-query = "select * fro mlt_transaction_items where x = y and item_id = 182373"
+query = "select * fro mlt_transaction_items where x = y and item_id = 4321"
+
+query2 = "select part_num from order_itemns"
